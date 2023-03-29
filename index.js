c929ba053bb22f0723b4020d752ec9b018b814ce
Nathan Stoltenberg <nds4@pdx.edu>
2023-03-29 10:34:03 -0700

    update mongoose calls to use exec

--- index.js
+++ index.js
@@ -1,187 +1,207 @@
-//https://cs300project.herokuapp.com/
+const express = require("express");
+const app = express();
+const path = require("path");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+const parser = require("body-parser");
+const port = process.env.PORT || 8347;
 
-const express = require('express')
-const app = express()
-const path = require('path')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const session = require('express-session')
-const parser = require('body-parser')
-const port = process.env.PORT || 8347
+require("dotenv").config();
 
-require("dotenv").config()
+const mongoose = require("mongoose");
+const Users = require("./models/Users");
+const { connect } = require("http2");
 
-const mongoose = require("mongoose")
-const Users = require('./models/Users')
-const { connect } = require('http2')
-mongoose.connect(process.env.DATABASE, {useUnifiedTopology: true, useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 mongoose.connection.on("error", (err) => {
-    console.log("ERROR: " + err.message)
-})
+  console.log("ERROR: " + err.message);
+});
 
 mongoose.connection.once("open", () => {
-    console.log("MONGODB CONNECTED")
-})
+  console.log("MONGODB CONNECTED");
+});
 
-require("./models/Message")
-require("./models/Users")
-require("./models/Group")
-const Message = mongoose.model("Message")
-const U = mongoose.model("User")
-const Group = mongoose.model("Group")
-
+require("./models/Message");
+require("./models/Users");
+require("./models/Group");
+const Message = mongoose.model("Message");
+const U = mongoose.model("User");
+const Group = mongoose.model("Group");
 
 passport.serializeUser(U.serializeUser());
 passport.deserializeUser(U.deserializeUser());
+app.use(
+  session({
+    secret: "Welcome",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(session({
-  secret:"Welcome",
-  resave: false,
-  saveUninitialized: false,
-}))
 
-passport.use(new LocalStrategy(U.authenticate()))
+passport.use(new LocalStrategy(U.authenticate()));
 
-app.use(parser.urlencoded(
-  {extended:true}
-))
+app.use(parser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/login.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
 });
 
-app.post('/login', passport.authenticate('local', {successRedirect: '/index', failureRedirect: '/'}),
-  (req, res) => {
-    
-});
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/index",
+    failureRedirect: "/",
+  }),
+  (req, res) => {}
+);
 
-app.post('/register', (req, res) => {
-  console.log("Username: " + req.body.username)
-  console.log("Password: " + req.body.password)
-  U.register(new U({username: req.body.username,}), req.body.password, (err, user) => {
-    if(err){
-      console.log(err)
-      res.sendFile(__dirname + '/login.html')
+app.post("/register", (req, res) => {
+  console.log("Username: " + req.body.username);
+  console.log("Password: " + req.body.password);
+  U.register(
+    new U({ username: req.body.username }),
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.sendFile(__dirname + "/login.html");
+      }
+      passport.authenticate("local")(req, res, () => {
+        res.sendFile(__dirname + "/index.html");
+      });
     }
-    passport.authenticate("local")(req, res, () => {
-      res.sendFile(__dirname + '/index.html')
-    })
-  })
-})
-
-app.get('/index',(req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  );
 });
 
-app.get('/groupchat', (req, res) => {
-  res.sendFile(__dirname + '/groupchat.html');
+app.get("/index", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/logout', (req, res) => {
+app.get("/groupchat", (req, res) => {
+  res.sendFile(__dirname + "/groupchat.html");
+});
+
+app.get("/logout", (req, res) => {
   req.logout();
-  res.sendFile(__dirname + '/login.html');
-})
+  res.sendFile(__dirname + "/login.html");
+});
 
 const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-const io = require('socket.io')(server)
+const io = require("socket.io")(server);
 
 io.use((socket, next) => {
   const user = socket.handshake.auth.user;
   socket.user = user;
   next();
-})
+});
 
-io.on('connection', (socket) => {
-    console.log('a user connected ' + socket.id + ' ' + socket.user);
+io.on("connection", (socket) => {
+  console.log("a user connected " + socket.id + " " + socket.user);
 
-    Message.find((err, data) => {
-      if(err)
-        console.log(err)
-      else
-        socket.emit('load', data)
+  Message.find()
+    .exec()
+    .then((data) => {
+      socket.emit("load", data);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 
-    Group.find((err, data) => {
-      if(err)
-        console.log(err)
-      else
-        socket.emit('group load', data)
+  Group.find()
+    .exec()
+    .then((data) => {
+      socket.emit("group load", data);
     })
+    .catch((err) => {
+      console.log(err);
+    });
 
-    U.find((err, data) => {
-      if(err)
-        console.log(err)
-      else
-        socket.emit("user load", data)
+  U.find()
+    .exec()
+    .then((data) => {
+      socket.emit("user load", data);
     })
+    .catch((err) => {
+      console.log(err);
+    });
 
-    socket.broadcast.emit("user connected", {
-      userID: socket.id,
-      username: socket.user,
-    })
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.user,
+  });
 
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-
-        const newMessage = new Message({
-            message: `${msg.user}: ${msg.msg}`
-        })
+    const newMessage = new Message({
+      message: `${msg.user}: ${msg.msg}`,
+    });
     newMessage.save();
-      });
+  });
 
-      socket.on("group add", (value) => {
-        console.log(value)
-        const newGroup = new Group({
-          groupname: `${value}`
-        })
-        newGroup.save();
-        socket.broadcast.emit("server group", value);
-      });
+  socket.on("group add", (value) => {
+    console.log(value);
+    const newGroup = new Group({
+      groupname: `${value}`,
+    });
+    newGroup.save();
+    socket.broadcast.emit("server group", value);
+  });
 
-      socket.on("group change", (value) => {
-        console.log(value)
-        socket.join(`${value}`)
-        Group.findOne({groupname: `${value}`}, (err, data) => {
-          if(err)
-            console.log(err)
-          else
-            socket.emit("groupm load", data)
-        })
+  socket.on("group change", (value) => {
+    console.log(value);
+    socket.join(`${value}`);
+    Group.findOne({ groupname: `${value}` })
+      .exec()
+      .then((data) => {
+        socket.emit("groupm load", data);
       })
-
-      socket.on('group message', (msg) => {
-        Group.updateOne({groupname: msg.group},
-        {$addToSet: {messages: [`${msg.user}: ${msg.msg}`] } }, (err, result) => {
-          if(err)
-            console.log(err)
-          else
-            console.log(result)
-        }
-      )
-        io.sockets.in(`${msg.group}`).emit('group message', msg);
-      })
-
-      socket.on("direct select", (sender, reciever) => {
-        console.log(sender + " -> " + reciever)
-        U.findOne({username: `${sender}`}, (err, data) => {
-          if(err)
-            console.log(err)
-          else
-            socket.emit("direct select", data);
-        })
-      })
-
-      socket.on("dsend", (sender, reciever, msg) => {
-      })
-
-      socket.on('disconnect', () => {
-        io.emit('disconnected', socket.user)     
-        console.log(`${socket.user} disconnected`);
+      .catch((err) => {
+        console.log(err);
       });
   });
+
+  socket.on("group message", (msg) => {
+    Group.updateOne(
+      { groupname: msg.group },
+      { $addToSet: { messages: [`${msg.user}: ${msg.msg}`] } }
+    )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    io.sockets.in(`${msg.group}`).emit("group message", msg);
+  });
+
+  socket.on("direct select", (sender, reciever) => {
+    console.log(sender + " -> " + reciever);
+    U.findOne({ username: `${value}` })
+      .exec()
+      .then((data) => {
+        socket.emit("private load", data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  socket.on("dsend", (sender, reciever, msg) => {});
+
+  socket.on("disconnect", () => {
+    io.emit("disconnected", socket.user);
+    console.log(`${socket.user} disconnected`);
+  });
+});
--- models/Users.js
+++ models/Users.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
-const passportLocalMongoose = require('passport-local-mongoose')
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-var user = {name: "", messages: [String]}
+var user = { name: String, messages: [String] };
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        minLength: 3
-    },
-    password: {
-        type: String,
-    },
-    contacts: [user]
-})
+  username: {
+    type: String,
+    minLength: 3,
+  },
+  password: {
+    type: String,
+  },
+  contacts: [user],
+});
 
-userSchema.plugin(passportLocalMongoose)
-module.exports = mongoose.model("User", userSchema)
+userSchema.plugin(passportLocalMongoose);
+module.exports = mongoose.model("User", userSchema);
